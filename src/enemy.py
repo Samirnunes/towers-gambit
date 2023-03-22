19e913366f7a64ae8a4c42352be5365803f6ea9c
Gabriel Telles <gabrieltellesm@gmail.com>
2023-03-22 13:37:39 -0300

    add enemy collide

--- src/enemy.py
+++ src/enemy.py
@@ -11,33 +11,44 @@ class Enemy:
         self.height = 0
         self.animation_count = 0
         self.health = 2
+        self.velocity = 1
         self.path = [(5, 195), (151, 195), (150, 284), (64, 287), (62, 454), (149, 459), (151, 545), (110, 547), 
                      (107, 633), (442, 636), (440, 549), (315, 546), (309, 459), (527, 459), (531, 591), (614, 590), 
                      (611, 374), (652, 372), (653, 198), (613, 198), (608, 157), (523, 155), (523, 113), (348, 109), 
                      (347, 192), (391, 194), (395, 328)]
-        self.img = None
     
     def draw(self, window):
         '''
         Draws the enemy with the given images.
         '''
-        self.animation_count +=1
-        self.img = self.imgs[self.animation_count]
+        self.animation_count += 1
+        img = self.imgs[self.animation_count]
         
         if self.animation_count >= len(self.imgs):
             self.animation_count = 0
             
-        window.blit(self.img, (self.x, self.y))
-        self.move()
+        window.blit(img, (self.x, self.y))
         
-    def collide(self, X, Y):
+    def collide(self, other_x, other_y, other_width, other_height):
         '''
         Returns if position has hit enemy.
         '''
+        rect1_left = self.x - self.width/2
+        rect1_right = self.x + self.width/2
+        rect1_top = self.y + self.height/2
+        rect1_bottom = self.y - self.height/2
         
-        if X <= self.x + self.width and X >= self.x:
-            if Y <= self.y + self.height and Y >= self.y:
-                return True        
+        rect2_left = other_x - other_width/2
+        rect2_right = other_x + other_width/2
+        rect2_top = other_y + other_height/2
+        rect2_bottom = other_y - other_height/2
+        
+        if ((rect1_right > rect2_left) and
+            (rect1_left < rect2_right) and
+            (rect1_bottom < rect2_top) and
+            (rect1_top > rect2_bottom)):
+          return True
+        
         return False
         
     def move(self):
@@ -45,12 +56,13 @@ class Enemy:
         Moves enemy.
         '''
         
-        pass
+        
     
-    def hit(self):
+    def hit(self, damage):
         '''
         Returns if an enemy has died and for each call removes 1 HP.
         '''
-        self.health -= 1
-        if self.health <= 0:
-            return True
+        self.health -= damage
+          
+    def dead(self):
+        return self.health <= 0
