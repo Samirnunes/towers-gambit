7f8abe56032815d444ea728a8b620b422dacc60d
Samir Nunes <samir.silva12342@gmail.com>
2023-04-05 20:16:39 -0300

    add graphic button creation

--- src/player.py
+++ src/player.py
@@ -10,28 +10,43 @@ class Player:
         self.money += money
 
     def display_user_interface(self, window, mouse_pos):
+        '''
+        Displays the UI.
+        '''
         pygame.font.init()
+        self.display_player_labels(window)
+        self.display_button(window, mouse_pos, 'Buy Bishop',
+                            BUY_BUTTON_FONT, BUY_BUTTON_FONT_COLOR, BUY_BUTTON_FONT_SIZE,
+                            BUY_BUTTON_POSITION, BUY_BUTTON_DIMENSIONS,
+                            BUY_BUTTON_LIGHT_COLOR, BUY_BUTTON_DARK_COLOR)
 
-        # Player labels
-
+    def display_player_labels(self, window):
+        '''
+        Displays the player's labels: lives and money.
+        '''
         label_font = pygame.font.SysFont(LABEL_FONT, LABEL_FONT_SIZE)
         lives_label = label_font.render(f'Lives: {self.lives}', 1, LABEL_FONT_COLOR)
         money_label = label_font.render(f'Money: {self.money}', 1, LABEL_FONT_COLOR)
         window.blit(lives_label, LIVES_LABEL_POSITION)
         window.blit(money_label, MONEY_LABEL_POSITION)
 
-        # Player buttons
-        
-        x_inside_button = (BUY_BUTTON_POSITION[0] <= mouse_pos[0] <= BUY_BUTTON_POSITION[0] + BUY_BUTTON_DIMENSIONS[0])
-        y_inside_button = (BUY_BUTTON_POSITION[1] <= mouse_pos[1] <= BUY_BUTTON_POSITION[1] + BUY_BUTTON_DIMENSIONS[1])
+    def display_button(self, window, mouse_pos, button_text, 
+                       button_font, button_font_color, button_font_size,
+                       button_position, button_dimensions, 
+                       button_light_color, button_dark_color):
+        '''
+        Displays a button of the user interface.
+        '''
+        x_inside_button = (button_position[0] <= mouse_pos[0] <= button_position[0] + button_dimensions[0])
+        y_inside_button = (button_position[1] <= mouse_pos[1] <= button_position[1] + button_dimensions[1])
         mouse_inside_button = x_inside_button and y_inside_button
         if mouse_inside_button:
-            pygame.draw.rect(window, BUY_BUTTON_LIGHT_COLOR, list(BUY_BUTTON_POSITION) + list(BUY_BUTTON_DIMENSIONS))
+            pygame.draw.rect(window, button_light_color, list(button_position) + list(button_dimensions))
         else:
-            pygame.draw.rect(window, BUY_BUTTON_DARK_COLOR, list(BUY_BUTTON_POSITION) + list(BUY_BUTTON_DIMENSIONS))
-        buy_font = pygame.font.SysFont(BUY_BUTTON_FONT, BUY_BUTTON_FONT_SIZE)
-        buy_text = buy_font.render(f'Buy a Bishop', 1, BUY_BUTTON_FONT_COLOR)
-        window.blit(buy_text, BUY_BUTTON_POSITION)
+            pygame.draw.rect(window, button_dark_color, list(button_position) + list(button_dimensions))
+        font = pygame.font.SysFont(button_font,button_font_size)
+        text = font.render(button_text, 1, button_font_color)
+        window.blit(text, button_position)
 
     def damage(self):
         if self.lives > 0:
