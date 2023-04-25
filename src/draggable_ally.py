7cf2e8cb742cbd66c6b9a380cf98590e810e2006
Samir Nunes <samir.silva12342@gmail.com>
2023-04-25 12:46:12 -0300

    add is_empty_pos analysis to draggable

--- src/draggable_ally.py
+++ src/draggable_ally.py
@@ -17,8 +17,14 @@ class DraggablePiece(Entity):
         self.pos[0] = GAME.TILE_DISCRETE_DIMENSION * (self.pos[0] // GAME.TILE_DISCRETE_DIMENSION)
         self.pos[1] = GAME.TILE_DISCRETE_DIMENSION * (self.pos[1] // GAME.TILE_DISCRETE_DIMENSION)
 
+    def is_empty_pos(self):
+        for ally in self.game.allies.get_entities():
+            if self.pos == ally.pos:
+                return False
+        return True
+
     def on_click(self):
-        if self.game.player.money >= self.piece.COST:
+        if self.game.player.money >= self.piece.COST and self.is_empty_pos():
             Piece(self.game, self.pos, self.piece)
             self.game.player.decrease_money(self.piece.COST)
         self.game.draggables.remove(self)
