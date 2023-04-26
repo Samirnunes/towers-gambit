ac8b5d8ee15601738d427e85b97f39119b3bdeb6
Gabriel Telles <gabrieltellesm@gmail.com>
2023-04-26 00:33:18 -0300

    add interface_allies doc

--- src/interface_allies.py
+++ src/interface_allies.py
@@ -6,15 +6,26 @@ from constants import INTERFACE_ALLIES
 
 class InterfaceAllies:
     def __init__(self, game):
+        '''
+        Initializes an instance of InterfaceAllies.
+
+        Parameters:
+            game (Game): an instance of the Game class
+        '''
         self.game = game
 
     def draw(self):
+        '''
+        Draws the ally images and their respective costs on the interface.
+        '''
+        # Draw ally images
         index = 0
         for image in INTERFACE_ALLIES.IMAGES:
             image = pygame.transform.scale(image, INTERFACE_ALLIES.IMAGE_SIZE)
             self.game.window.blit(image, INTERFACE_ALLIES.IMAGE_POSITIONS[index])
             index += 1
         
+        # Draw ally costs
         index = 0
         for text in INTERFACE_ALLIES.COST_TEXTS:
             label_font = pygame.font.SysFont(INTERFACE_ALLIES.LABEL_FONT, INTERFACE_ALLIES.LABEL_FONT_SIZE)
