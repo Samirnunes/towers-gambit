f9c3447ec5cea43d879670fe0f21463a986f356f
Gabriel Telles <gabrieltellesm@gmail.com>
2023-04-26 00:32:09 -0300

    add interface doc

--- src/interface.py
+++ src/interface.py
@@ -8,14 +8,29 @@ from constants import INTERFACE
 
 class Interface:
     def __init__(self, game):
+        """
+        Initializes the Interface class.
+
+        Parameters:
+            game: the game instance
+        """
         self.game = game
+
+        # Creates buttons for each type of piece the player can buy
         self.buttons = Entities();
         for BUTTON in INTERFACE.BUTTONS:
             self.buttons.append(BuyPieceButton(game, BUTTON))
+
+        # Initializes the labels for displaying lives and money
         self.labels = Labels(game)
+
+        # Initializes the interface for displaying the allied pieces
         self.interface_allies = InterfaceAllies(game)
 
     def update(self):
+        """
+        Updates the UI buttons
+        """
         self.buttons.update()
 
     def draw(self):
