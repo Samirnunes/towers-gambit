6beeaf6b02337d54ccacd4dd7fb706dc19fc77b8
Gabriel Telles <gabrieltellesm@gmail.com>
2023-04-26 00:30:43 -0300

    add labels doc

--- src/labels.py
+++ src/labels.py
@@ -4,6 +4,12 @@ from constants import LABELS
 
 class Labels():
     def __init__(self, game):
+        """
+        Initializes a Labels object.
+
+        Args:
+            game (Game): The Game object.
+        """
         self.game = game
 
     def draw(self):
@@ -11,10 +17,16 @@ class Labels():
         Displays the player's labels: lives and money.
         '''
         label_font = pygame.font.SysFont(LABELS.LABEL_FONT, LABELS.LABEL_FONT_SIZE)
+
+        # Renders lives and money labels
         lives_label = label_font.render(f'Lives: {self.game.player.lives}', 1, LABELS.LABEL_FONT_COLOR)
         money_label = label_font.render(f'Money: {self.game.player.money}', 1, LABELS.LABEL_FONT_COLOR)
+
+        # Loads and scales the background image for the labels
         image = pygame.image.load(os.path.join('assets', 'user_interface', 'user_labels.png'))
         image = pygame.transform.scale(image, LABELS.LABEL_BACKGROUND_SIZE)
+
+        # Displays the background image and the labels
         self.game.window.blit(image, LABELS.LABEL_BACKGROUND_POSITION)
         self.game.window.blit(lives_label, LABELS.LIVES_LABEL_POSITION)
         self.game.window.blit(money_label, LABELS.MONEY_LABEL_POSITION)
